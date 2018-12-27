7332a9f626017d48b19a7c7ddb6c12540434c72a
Christian Garbs <mitch@cgarbs.de>
2018-12-27 12:51:27 +0100

    add simple calculation to helloworld example
    we want something that can be unit-tested

--- cobol/helloworld/src/helloworld.cbl
+++ cobol/helloworld/src/helloworld.cbl
@@ -1,6 +1,23 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. HELLOWORLD.
 
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 SUMMAND-1    PIC 9(04) VALUE 1.
+       01 SUMMAND-2    PIC 9(04) VALUE 1.
+       01 RESULT       PIC 9(04).
+
        PROCEDURE DIVISION.
            DISPLAY "Hello, world!"
+           PERFORM U01-CALCULATION
+           PERFORM U02-SHOW-RESULT
+           GOBACK
+           .
+
+       U01-CALCULATION SECTION.
+           ADD SUMMAND-1 TO SUMMAND-2 GIVING RESULT
+           .
+
+       U02-SHOW-RESULT SECTION.
+           DISPLAY SUMMAND-1 " + " SUMMAND-2 " = " RESULT
            .
