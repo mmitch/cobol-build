4dc777b3ec769057f0522c078354a33b8a92b37b
Christian Garbs <mitch@cgarbs.de>
2019-02-18 19:22:01 +0100

    new syntax: BUILD BINARY binary USING source1 source2 ...

--- cobol/genmk.sh
+++ cobol/genmk.sh
@@ -5,10 +5,62 @@ abend()
     exec 1>&2
     HAVE_TPUT="$(command -v tput || true)"
     [ "$HAVE_TPUT" ] && tput setaf 1 && tput bold
-    echo "$*"
+    echo "$0: $*"
     [ "$HAVE_TPUT" ] && tput sgr0
     exit 1
 }
+
+parse_build()
+{
+    TYPE="$1"
+    shift
+
+    case "$TYPE" in
+
+	BINARY|binary)
+	    TARGET="$1"
+	    LINKFLAG=-x
+	    TYPE=BINARY
+	    ;;
+
+	MODULE|module)
+	    TARGET="$1.so"
+	    LINKFLAG=-b
+	    TYPE=MODULE
+	    ;;
+
+	*)
+	    abend "unknown build target type: $TYPE"
+	;;
+    esac
+    shift
+
+    [ "$1" = USING ] || abend "expected USING, but got: $2"
+    shift
+
+    echo -n "\$(TARGETDIR)/$TARGET:"
+    FIRSTOBJECT=""
+    for SOURCE in "$@"; do
+	OBJECT="\$(BUILDDIR)/${SOURCE%.*}.o"
+	OBJECTS[$OBJECT]="$SOURCE"
+	echo -n " $OBJECT"
+
+	if [ -z "$FIRSTOBJECT" ]; then
+	    FIRSTOBJECT="$OBJECT"
+	fi
+    done
+    echo
+    echo "	\$(COBC) $LINKFLAG \$(COBFLAGS) -o \$@ \$^"
+    echo
+    TARGETS[$TARGET]="$*"
+
+    if [ $TYPE = BINARY ]; then
+	# main program needs both -c and -x, otherwise <undefined reference to `main'>
+	OBJECTFLAGS[$FIRSTOBJECT]="-x"
+    fi
+}
+
+###########
 
 if [ "${BASH_VERSION%%.*}" -lt 4 ]; then
     abend 'need at least bash v4 for associative arrays'
@@ -17,7 +69,7 @@ fi
 echo ".PHONY: build test prepare-test"
 echo
 
-declare -A TARGETS OBJECTS MAINS TESTS
+declare -A TARGETS OBJECTS OBJECTFLAGS TESTS
 
 while IFS= read -r LINE; do
 
@@ -29,49 +81,15 @@ while IFS= read -r LINE; do
 
     set -- $LINE
 
-    COMMAND="$1"
+    VERB="$1"
     shift
     
-    case "$COMMAND" in
-	
-	BINARY|binary)
-	    TARGET="$1"
-	    shift 2
-	    echo -n "\$(TARGETDIR)/$TARGET:"
-	    MAIN=""
-	    for SOURCE in "$@"; do
-		OBJECT="\$(BUILDDIR)/${SOURCE%.*}.o"
-		OBJECTS[$OBJECT]="$SOURCE"
-		echo -n " $OBJECT"
+    case "$VERB" in
 
-		if [ -z "$MAIN" ]; then
-		    MAIN="$OBJECT"
-		fi
-	    done
-	    echo
-	    echo '	$(COBC) -x $(COBFLAGS) -o $@ $^'
-	    echo
-	    TARGETS[$TARGET]="$*"
+	BUILD|build)
+	    parse_build "$@"
+	    ;;
 
-	    # main program needs both -c and -x, otherwise <undefined reference to `main'>
-	    MAINS[$MAIN]="-x"
-	    ;;
-	
-	MODULE|module)
-	    TARGET="$1.so"
-	    shift 2
-	    echo -n "\$(TARGETDIR)/$TARGET:"
-	    for SOURCE in "$@"; do
-		OBJECT="\$(BUILDDIR)/${SOURCE%.*}.o"
-		OBJECTS[$OBJECT]="$SOURCE"
-		echo -n " $OBJECT"
-	    done
-	    echo
-	    echo '	$(COBC) -b $(COBFLAGS) -o $@ $^'
-	    echo
-	    TARGETS[$TARGET]="$*"
-	    ;;
-	
 	TEST|test)
 	    TEST="$1"
 	    shift 2
@@ -93,7 +111,7 @@ EOF
 	    ;;
 	
 	*)
-	    abend "$0: unknown command word: $COMMAND"
+	    abend "unknown verb: $VERB"
 	;;
     esac
     
@@ -101,9 +119,9 @@ done
 
 for OBJECT in ${!OBJECTS[*]}; do
     SOURCE="${OBJECTS[$OBJECT]}"
-    MAIN="${MAINS[$OBJECT]}"
+    OBJECTFLAGS="${OBJECTFLAGS[$OBJECT]}"
     echo "$OBJECT: \$(SOURCEDIR)/$SOURCE"
-    echo "	\$(COBC) -c $MAIN \$(COBFLAGS) -o \$@ \$<"
+    echo "	\$(COBC) -c $OBJECTFLAGS \$(COBFLAGS) -o \$@ \$<"
     echo
 done
 
